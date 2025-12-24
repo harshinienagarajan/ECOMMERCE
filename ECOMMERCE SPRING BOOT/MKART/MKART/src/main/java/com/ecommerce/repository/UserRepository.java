1edf271f255f91ff0a980e0d3afd5f80e494e4e4
harshinie <kit28.24bcb017@gmail.com>
2025-12-24 08:55:19 +0530

    Add files via upload

---
+++ ECOMMERCE SPRING BOOT/MKART/MKART/src/main/java/com/ecommerce/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.ecommerce.repository;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.ecommerce.entity.User;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByUsername(String username);
+    public Optional<User> findByEmail(String email);
+    public Boolean existsByUsername(String username);
+    public Boolean existsByEmail(String email);
+    @Query("SELECT u FROM User u WHERE u.username = ?1 OR u.email = ?2")
+    public Optional<User> findByUsernameOrEmail(String username, String email);
+
+}
